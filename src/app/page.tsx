7d2548f5888e244caffc5cd62fd3ed2d01c5efbc
Marco Haber <marcoshaber99@gmail.com>
2024-10-07 15:48:00 +0300

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-500 text-2xl font-bold">Hello World</h1>
+      <Button size="lg">Click me</Button>
+    </div>
+  );
+}
